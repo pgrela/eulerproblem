11588f1eb4db15bfee152115dfc6e953739bbc1f
Patryk Grela <patryk.grela@payu.pl>
2014-10-07 19:18:54 +0200

    problems 62 in BigDecimals

--- src/main/java/pgrela/eulerproblem/problem62/CubicPermutations.java
+++ src/main/java/pgrela/eulerproblem/problem62/CubicPermutations.java
@@ -2,13 +2,13 @@ package pgrela.eulerproblem.problem62;
 
 import static pgrela.eulerproblem.common.SolutionRunner.printSolution;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.LongStream;
 
 import pgrela.eulerproblem.common.EulerSolver;
-import pgrela.eulerproblem.common.Longs;
 
 public class CubicPermutations implements EulerSolver {
 
@@ -18,32 +18,61 @@ public class CubicPermutations implements EulerSolver {
         printSolution(CubicPermutations.class);
     }
 
-    public long solve() {
-        Map<Long, Integer> counter = new HashMap<>();
-        long winner = LongStream.range(0, LONG).sequential()
-                .map(i -> i * i * i)
-                //.filter(i->orderDigitsInNumber(i)==125).peek(System.out::println)
-                .map(CubicPermutations::orderDigitsInNumber)
+    public String solveToString() {
+        Map<ComparableCharArray, Integer> counter = new HashMap<>();
+        ComparableCharArray winner = LongStream.range(0, LONG).sequential()
+                .mapToObj(i -> BigDecimal.valueOf(i).pow(3))
+                .map(ComparableCharArray::new)
                 .peek(i -> counter.put(i, counter.getOrDefault(i, 0) + 1))
-                .filter(i -> counter.get(i) == 4)
-                .findFirst().getAsLong();
+                .filter(i -> counter.get(i) == 5)
+                .findFirst().get();
 
         return LongStream.range(0, LONG)
-                .map(i -> i * i * i)
-                .filter(i -> orderDigitsInNumber(i) == winner)
-                .findFirst().getAsLong();
+                .mapToObj(i -> BigDecimal.valueOf(i).pow(3))
+                .filter(i -> new ComparableCharArray(i).equals(winner))
+                .findFirst().get().toString();
     }
+    public static class ComparableCharArray{
 
-    public static long orderDigitsInNumber(long i) {
-        int[] digits = Longs.toDigitArray(i);
-        Arrays.sort(digits);
-        for (int j = 0; j < digits.length / 2; j++) {
-            int a = digits[j];
-            int other = digits.length - 1 - j;
-            digits[j] = digits[other];
-            digits[other] = a;
+        private int[] ints;
+
+        public BigDecimal getB() {
+            return b;
         }
-        return Longs.fromDigitArray(digits);
+
+        private BigDecimal b;
+
+        public ComparableCharArray(BigDecimal b) {
+            this.b = b;
+            this.ints = b.toString().chars().sorted().toArray();
+            for (int i = 0; i < ints.length/2; i++) {
+                int k=ints.length-1-i;
+                int o=ints[i];
+                ints[i]=ints[k];
+                ints[k]=o;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ComparableCharArray)) return false;
+
+            ComparableCharArray that = (ComparableCharArray) o;
+
+            return Arrays.equals(ints, that.ints);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(ints);
+        }
+
+        @Override
+        public String toString() {
+            return b.toString();
+        }
     }
 }
 
