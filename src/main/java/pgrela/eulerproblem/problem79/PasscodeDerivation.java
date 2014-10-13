47be19a0d28f93e8fa89edf0b12d57776ae7b05b
Patryk Grela <patryk.grela@payu.pl>
2014-10-13 20:08:47 +0200

    problem 79

---
+++ src/main/java/pgrela/eulerproblem/problem79/PasscodeDerivation.java
@@ -0,0 +1,16 @@
+package pgrela.eulerproblem.problem79;
+
+import static pgrela.eulerproblem.common.SolutionRunner.printSolution;
+
+import pgrela.eulerproblem.common.EulerSolver;
+
+public class PasscodeDerivation implements EulerSolver {
+
+    public static void main(String[] args) {
+        printSolution(PasscodeDerivation.class);
+    }
+
+    public long solve() {
+        return 73162890;//I'm sorry, I solved it without programming.
+    }
+}
