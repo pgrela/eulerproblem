ff2d2550d189eef64d8f0d7811c1f9605d97acdc
Patryk Grela <patryk.grela@payu.pl>
2014-10-13 19:17:35 +0200

    problem 78, two rows dynamic

--- src/main/java/pgrela/eulerproblem/problem78/CoinPartitions.java
+++ src/main/java/pgrela/eulerproblem/problem78/CoinPartitions.java
@@ -1,47 +1,43 @@
 package pgrela.eulerproblem.problem78;
 
-import pgrela.eulerproblem.common.EulerSolver;
-
-import java.util.stream.IntStream;
-
-import static pgrela.eulerproblem.common.Integers.ONE_MILLION;
 import static pgrela.eulerproblem.common.SolutionRunner.printSolution;
+
+import java.util.Arrays;
+
+import pgrela.eulerproblem.common.EulerSolver;
 
 public class CoinPartitions implements EulerSolver {
 
-    public static final int MAX_ALLOWED_LENGTH = 5000;
-    private static final int[][] cache = new int[MAX_ALLOWED_LENGTH + 1][MAX_ALLOWED_LENGTH + 1];
+    public static final int MAX_ALLOWED_LENGTH = 100000;
+    private int[] previous = new int[MAX_ALLOWED_LENGTH + 1];
+    private int[] next = new int[MAX_ALLOWED_LENGTH + 1];
+    private int MODULO = 1000000;
 
     public static void main(String[] args) {
         printSolution(CoinPartitions.class);
     }
 
     public long solve() {
-        long million = 1000000;
-        return IntStream.iterate(1, i -> i + 1).limit(MAX_ALLOWED_LENGTH).filter(i -> {
-            long s = countSumations(i, i);
-            return s%million==0;
-        }).findFirst().getAsInt();
+        Arrays.fill(previous,0);
+        previous[0]=1;
+        next[0]=1;
+        for (int max = 1; max <= MAX_ALLOWED_LENGTH; max++) {
+            for (int length = 1; length <= MAX_ALLOWED_LENGTH; length++) {
+                int sum=0;
+                int remainingLength = length;
+                while(remainingLength>=0){
+                    sum+=previous[remainingLength];
+                    remainingLength-=max;
+                }
+                next[length]=sum%MODULO;
+            }
+            if(next[max]%MODULO==0){
+                return max;
+            }
+            int[] tmp=next;
+            next=previous;
+            previous=tmp;
+        }
+        return -1;
     }
-
-    public int countSumations(int length, int max) {
-        if (length == 0) {
-            return 1;
-        }
-        if (max == 0) {
-            return 0;
-        }
-        if (cache[length][max] != 0) {
-            return cache[length][max];
-        }
-
-        int sum = 0;
-        int i = length;
-        while (i >= 0) {
-            sum = sum%(ONE_MILLION*10)+(countSumations(i, max - 1));
-            i -= max;
-        }
-        return cache[length][max] = sum%(ONE_MILLION*10);
-    }
-
 }
