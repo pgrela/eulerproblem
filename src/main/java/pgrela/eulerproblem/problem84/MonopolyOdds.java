81bac8c70c205e943c0e91bde9632b4fa0d84fdf
Patryk Grela <patrykgrela@gmail.com>
2014-10-23 10:17:09 +0200

    problem 84, new class

---
+++ src/main/java/pgrela/eulerproblem/problem84/MonopolyOdds.java
@@ -0,0 +1,16 @@
+package pgrela.eulerproblem.problem84;
+
+import pgrela.eulerproblem.common.EulerSolver;
+
+import static pgrela.eulerproblem.common.SolutionRunner.printSolution;
+
+public class MonopolyOdds implements EulerSolver {
+
+    public static void main(String[] args) {
+        printSolution(MonopolyOdds.class);
+    }
+
+    public long solve() {
+        return 0;
+    }
+}
