aff97128d0cafbcfad54985751e6793c84b2d08b
Patryk Grela <patrykgrela@gmail.com>
2016-03-21 23:34:21 +0100

    problem 127, still brute but does the job

--- src/main/java/pgrela/eulerproblem/problem127/ABCHits.java
+++ src/main/java/pgrela/eulerproblem/problem127/ABCHits.java
@@ -1,11 +1,11 @@
 package pgrela.eulerproblem.problem127;
 
 import pgrela.eulerproblem.common.EulerSolver;
-import pgrela.eulerproblem.common.Maths;
 import pgrela.eulerproblem.common.Primes;
 
-import java.util.stream.IntStream;
-
+import static java.util.stream.LongStream.range;
+import static java.util.stream.LongStream.rangeClosed;
+import static pgrela.eulerproblem.common.Maths.gcd;
 import static pgrela.eulerproblem.common.SolutionRunner.printSolution;
 
 public class ABCHits implements EulerSolver {
@@ -13,20 +13,43 @@ public class ABCHits implements EulerSolver {
         printSolution(ABCHits.class);
     }
 
+    public static final int MAX = 120000;
+
+    public static final int[] primes = Primes.primes(MAX).toArray();
+
     public long solve() {
-        return IntStream.range(1, 1000).map(c ->
-                (int)IntStream.range(1, c/2)
-                        .filter(b -> Maths.gcd(c, b) == 1)
-                        .filter(b -> Maths.gcd(c, c - b) == 1)
-                        .filter(b -> Maths.gcd(b, c - b) == 1)
-                        .filter(b -> Primes.getPrimeFactorsHistogram((c - b) * b * c, Primes.getPrimesToFactorizeInteger())
-                                .keySet()
-                                .stream()
-                                .reduce(1, (x, y) -> x * y) < c
-                        )
-                        .peek(a->System.out.println(a+" "+(c-a)+" "+c))
-                        .count()*c
-        ).sum();
+        long[] rads = range(0, MAX).map(i->rad((int)i)).toArray();
+        return range(1, MAX).parallel()
+                .filter(c -> !Primes.isPrime((int) c))
+                .map(c ->
+                        rangeClosed(1, c / 2).parallel()
+                                .filter(b -> rads[(int) (c - b)] * rads[(int) b] * rads[(int) c] < c && gcd(c, (c - b) * b) == 1 && gcd(c, c - b) == 1)
+                                .count() * c
+                ).sum();
+    }
+
+    public static int rad(int number) {
+        if (number < 1) return 0;
+        int rad = 1;
+        for (int prime : primes) {
+            int times = 0;
+            if (number % prime == 0) {
+                while (number % prime == 0) {
+                    number /= prime;
+                    ++times;
+                }
+                if (times > 0) {
+                    rad *= prime;
+                }
+            }
+            if (number == 1) {
+                return rad;
+            }
+            if (prime * prime > number) {
+                return rad * number;
+            }
+        }
+        return rad;
     }
 
 }
